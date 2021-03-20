2bd945e676d4fba0ad619666083eee950d14d590
Desmond Brown <browndesmond30@yahoo.com>
2021-03-20 10:53:39 +0000

    Added test for Lyrics models

--- lyrics/tests/test_models.py
+++ lyrics/tests/test_models.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from lyrics.models import Lyric
+
+
+User = get_user_model()
+
+
+class LyricsModelTest(TestCase):
+    def setUp(self):
+        self.lyric_author = User.objects.create(username="author@test.com", password="password@test.author")
+        self.lyric_data = {
+            "title": "Test Lyric Title",
+            "description": "Test Lyric description",
+            "content": "Test Lyric Content",
+            "author": self.lyric_author,
+            "composer": "Lyric Composer",
+        }
+
+    def test_object_creation(self):
+        """
+        Test that lyric object can be created and saved in the db
+        returns:
+        """
+        lyric = Lyric.objects.create(**self.lyric_data)
+        self.assertEqual(lyric.title, self.lyric_data["title"])
+        self.assertEqual(lyric.description, self.lyric_data["description"])
+        self.assertEqual(lyric.content, self.lyric_data["content"])
+        self.assertEqual(lyric.author, self.lyric_data["author"])
+        self.assertEqual(lyric.composer, self.lyric_data["composer"])
