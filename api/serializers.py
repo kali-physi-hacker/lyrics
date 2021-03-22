ef47aefa1f3ff089f4752b1633c2b110ef74b58d
Efoblack <xplicitbwoy212@gmail.com>
2021-03-22 00:37:54 +0000

    Removed serializers from api app

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest-framework import serializers
-
-from .models import Lyric
-
-class LyricSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lyric
-        fields = ['id', 'title', 'discription', 'content', 'author', 'composer', 'dated_created']
