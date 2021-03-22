1032631319f72cc9fa361ef8f4f9173121f335c2
rj-0605 <rojedkay@gmail.com>
2021-03-22 15:52:12 +0000

    modified api\views.py file

--- api/views.py
+++ api/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+from lyrics.models import Lyric
+from lyrics.serializers import LyricSerializer
+
+from django.http import HttpResponse, JsonResponse
+
 
 def test(request):
     template = ""
     context = {}
     return render(request=request, template_name=template, context=context)
+
+
+@api_view(["GET"])
+def Get_LyricsList(request): # new 
+
+
+    try :
+        # first pick info from model
+        lyrics = Lyric.objects.all()
+    except Lyric.DoesNotExist:
+        return HttpResponse(status=404)
+        
+        if request.method == "GET":
+            # serialize info
+            serializer = LyricSerializer(lyrics, many=True)
+            # return info 
+            return Response (serializer.data)
+
+
+
+    
+    # return info 
+    # check if info can be gotten at once and serialized too
+
+    
