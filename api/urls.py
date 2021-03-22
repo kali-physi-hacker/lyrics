8e481fdcc3e90d2d1d36ec259c0effb7dfe9bf4d
rj-0605 <rojedkay@gmail.com>
2021-03-22 15:43:21 +0000

    modified the urls.py of api app to include the views in \api\views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-from .views import test
+from .views import test , Get_LyricsList 
 
-urlpatterns = [path("/test/", test, name="test")]
+urlpatterns = [
+    path("/test/", test, name="test"),
+    path("/getlyricslist/", Get_LyricsList, name="get_lyricslist")
+
+]
