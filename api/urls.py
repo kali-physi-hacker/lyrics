5cfea0bd3fefeb11761f788cce92e0f83d102586
rj-0605 <rojedkay@gmail.com>
2021-03-24 20:15:58 +0000

    made changes to api\urls.py on api link path

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
 
-from .views import test , lyrics_list 
+from .views import test, lyrics_list
 
-urlpatterns = [
-    path("/test/", test, name="test"),\
-    path("/getlyricslist/", lyrics_list, name="lyrics_list")
-
-]
+urlpatterns = [path("/test/", test, name="test"), path("/get/lyrics", lyrics_list, name="lyrics_list")]
